b55e9cb560011ac52b3a2f79a944295be684b6b7
NealsonW <nealsonwilliam@gmail.com>
2022-03-21 20:08:05 +0700

    update database

--- data/yuk_bayar.sql
+++ data/yuk_bayar.sql
@@ -1,245 +1,138 @@
--- MariaDB dump 10.19  Distrib 10.4.22-MariaDB, for Win64 (AMD64)
---
--- Host: localhost    Database: yuk_bayar
--- ------------------------------------------------------
--- Server version	10.4.22-MariaDB
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `diskon`
---
-
-DROP TABLE IF EXISTS `diskon`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `diskon` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `id` int(6) NOT NULL,
   `kode` varchar(255) NOT NULL,
   `nama` varchar(255) NOT NULL,
-  `id_pengguna` int(6) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_pengguna` (`id_pengguna`),
-  CONSTRAINT `diskon_ibfk_1` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id`)
+  `id_pengguna` int(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `diskon`
---
-
-LOCK TABLES `diskon` WRITE;
-/*!40000 ALTER TABLE `diskon` DISABLE KEYS */;
-/*!40000 ALTER TABLE `diskon` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `kategorilayanan`
---
-
-DROP TABLE IF EXISTS `kategorilayanan`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `kategorilayanan` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `id` int(6) NOT NULL,
   `kategori` varchar(255) NOT NULL,
-  `id_varian` int(6) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_varian` (`id_varian`),
-  CONSTRAINT `kategorilayanan_ibfk_1` FOREIGN KEY (`id_varian`) REFERENCES `varian` (`id`)
+  `id_varian` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `kategorilayanan`
---
-
-LOCK TABLES `kategorilayanan` WRITE;
-/*!40000 ALTER TABLE `kategorilayanan` DISABLE KEYS */;
-/*!40000 ALTER TABLE `kategorilayanan` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `layanan`
---
-
-DROP TABLE IF EXISTS `layanan`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `layanan` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `id` int(6) NOT NULL,
   `jenis` varchar(255) NOT NULL,
   `nama` varchar(255) NOT NULL,
-  `id_kategorilayanan` int(6) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_kategorilayanan` (`id_kategorilayanan`),
-  CONSTRAINT `layanan_ibfk_1` FOREIGN KEY (`id_kategorilayanan`) REFERENCES `kategorilayanan` (`id`)
+  `id_kategorilayanan` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `layanan`
---
-
-LOCK TABLES `layanan` WRITE;
-/*!40000 ALTER TABLE `layanan` DISABLE KEYS */;
-/*!40000 ALTER TABLE `layanan` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `mitra`
---
-
-DROP TABLE IF EXISTS `mitra`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `mitra` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `id` int(6) NOT NULL,
   `alamat` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `noTelpon` varchar(255) NOT NULL,
   `pemilikBisnis` varchar(255) NOT NULL,
-  `id_layanan` int(6) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_layanan` (`id_layanan`),
-  CONSTRAINT `mitra_ibfk_1` FOREIGN KEY (`id_layanan`) REFERENCES `layanan` (`id`)
+  `id_layanan` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `mitra`
---
-
-LOCK TABLES `mitra` WRITE;
-/*!40000 ALTER TABLE `mitra` DISABLE KEYS */;
-/*!40000 ALTER TABLE `mitra` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `pengguna`
---
-
-DROP TABLE IF EXISTS `pengguna`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `pengguna` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `id` int(6) NOT NULL,
   `email` varchar(255) NOT NULL,
   `nama` varchar(255) NOT NULL,
   `noTelpon` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `saldoYukPay` int(10) NOT NULL,
-  `tipepengguna` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  `tipepengguna` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `pengguna`
---
-
-LOCK TABLES `pengguna` WRITE;
-/*!40000 ALTER TABLE `pengguna` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pengguna` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `transaksi`
---
-
-DROP TABLE IF EXISTS `transaksi`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `transaksi` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `id` int(6) NOT NULL,
   `totalHarga` int(10) NOT NULL,
   `id_pengguna` int(6) NOT NULL,
-  `id_layanan` int(6) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_pengguna` (`id_pengguna`),
-  KEY `id_layanan` (`id_layanan`),
-  CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id`),
-  CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_layanan`) REFERENCES `layanan` (`id`)
+  `id_layanan` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `transaksi`
---
-
-LOCK TABLES `transaksi` WRITE;
-/*!40000 ALTER TABLE `transaksi` DISABLE KEYS */;
-/*!40000 ALTER TABLE `transaksi` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `varian`
---
-
-DROP TABLE IF EXISTS `varian`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `varian` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `id` int(6) NOT NULL,
   `harga` int(10) NOT NULL,
-  `jenis` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  `jenis` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `varian`
---
-
-LOCK TABLES `varian` WRITE;
-/*!40000 ALTER TABLE `varian` DISABLE KEYS */;
-/*!40000 ALTER TABLE `varian` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `wallettopup`
---
-
-DROP TABLE IF EXISTS `wallettopup`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `wallettopup` (
-  `id` int(6) NOT NULL AUTO_INCREMENT,
+  `id` int(6) NOT NULL,
   `kodeYukPay` varchar(255) NOT NULL,
   `metode` varchar(255) NOT NULL,
   `nominal` int(10) NOT NULL,
-  `id_pengguna` int(6) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_pengguna` (`id_pengguna`),
-  CONSTRAINT `wallettopup_ibfk_1` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id`)
+  `id_pengguna` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `wallettopup`
---
+ALTER TABLE `diskon`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_pengguna` (`id_pengguna`);
 
-LOCK TABLES `wallettopup` WRITE;
-/*!40000 ALTER TABLE `wallettopup` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wallettopup` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+ALTER TABLE `kategorilayanan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_varian` (`id_varian`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+ALTER TABLE `layanan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_kategorilayanan` (`id_kategorilayanan`);
 
--- Dump completed on 2022-02-22 20:43:22
+ALTER TABLE `mitra`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_layanan` (`id_layanan`);
+
+ALTER TABLE `pengguna`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_pengguna` (`id_pengguna`),
+  ADD KEY `id_layanan` (`id_layanan`);
+
+ALTER TABLE `varian`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `wallettopup`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_pengguna` (`id_pengguna`);
+
+ALTER TABLE `diskon`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `kategorilayanan`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `layanan`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `mitra`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `pengguna`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `transaksi`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `varian`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `wallettopup`
+  MODIFY `id` int(6) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE `diskon`
+  ADD CONSTRAINT `diskon_ibfk_1` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id`);
+
+ALTER TABLE `kategorilayanan`
+  ADD CONSTRAINT `kategorilayanan_ibfk_1` FOREIGN KEY (`id_varian`) REFERENCES `varian` (`id`);
+
+ALTER TABLE `layanan`
+  ADD CONSTRAINT `layanan_ibfk_1` FOREIGN KEY (`id_kategorilayanan`) REFERENCES `kategorilayanan` (`id`);
+
+ALTER TABLE `mitra`
+  ADD CONSTRAINT `mitra_ibfk_1` FOREIGN KEY (`id_layanan`) REFERENCES `layanan` (`id`);
+
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id`),
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_layanan`) REFERENCES `layanan` (`id`);
+
+ALTER TABLE `wallettopup`
+  ADD CONSTRAINT `wallettopup_ibfk_1` FOREIGN KEY (`id_pengguna`) REFERENCES `pengguna` (`id`);
+COMMIT;

15f48edc33b1dee402ede7b9bc84d92ed084e017
Hilbert <blitherium@gmail.com>
2022-04-12 04:07:53 +0800

    Changed SQL implementation to Gorm

--- db/db.go
+++ db/db.go
@@ -3,11 +3,18 @@ package db
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
 )
 
 func Connect() *gorm.DB {
 	dsn := "root:@tcp(localhost:3306)/yuk_bayar?charset=utf8mb4&parseTime=true&loc=Local"
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true,
+		},
+		Logger: logger.Default.LogMode(logger.Info),
+	})
 	if err != nil {
 		panic(err.Error())
 	}
--- models/pengguna/controller.go
+++ models/pengguna/controller.go
@@ -3,12 +3,9 @@ package pengguna
 import (
 	//"errors"
 
-	"database/sql"
-	"fmt"
-	"log"
-
 	//"strconv"
 
+	"fmt"
 	"yukbayar-rpll-be/db"
 	"yukbayar-rpll-be/helpers/response"
 
@@ -17,97 +14,78 @@ import (
 
 func GetUserData(c *gin.Context) {
 	db := db.Connect()
-
 	id := c.Param("id")
 
-	query := "SELECT * FROM pengguna WHERE ID =" + id
-	//query := "SELECT * FROM pengguna"
+	var pengguna Pengguna
 
-	// if nama != "" {
-	// 	query += "WHERE nama='" + nama + "'"
-	// }
-
-	rows, err := db.Query(query)
-	if err != nil {
-		log.Println(err)
+	result := db.First(&pengguna, id)
+	if result.Error != nil {
+		response.SendErrorResponse(c, response.Response{
+			Message: "User ID not found",
+			Data:    result.Error,
+		})
+		return
 	}
-
-	var profile Pengguna
-	var profiles []Pengguna
-
-	for rows.Next() {
-		if err := rows.Scan(&profile.ID, &profile.Email, &profile.Nama, &profile.NoTelpon, &profile.Password, &profile.TglLahir, &profile.Gender, &profile.SaldoYukPay, &profile.TipePengguna); err != nil {
-			log.Fatal(err.Error())
-		} else {
-			profiles = append(profiles, profile)
-		}
-	}
-
-	fmt.Println(profiles)
 
 	var res response.Response
-	if len(profiles) > 0 {
-		res.Message = "Get User Data Success"
-		res.Data = profiles
-		response.SendSuccessResponse(c, res)
-	} else {
-		res.Message = "Get User Data Failed"
-		response.SendErrorResponse(c, res)
-	}
-
+	fmt.Println(pengguna.NoTelpon)
+	res.Message = "Get user data success"
+	res.Data = result.RowsAffected
+	response.SendSuccessResponse(c, res)
 }
 
 func UpdateUser(c *gin.Context) {
-
 	db := db.Connect()
-	defer db.Close()
 
+	email := c.PostForm("email")
 	nama := c.PostForm("nama")
-	pass := c.PostForm("password")
-	noTelp := c.PostForm("noTelpon")
+	noTelpon := c.PostForm("noTelpon")
+	password := c.PostForm("password")
+	// tglLahir := c.PostForm("tglLahir")
+	// gender = c.PostForm("gender")
+
 	id := c.Param("id")
 
-	var profile Pengguna
-	err := db.QueryRow("SELECT nama, noTelpon, password FROM pengguna WHERE id="+id).Scan(&profile.Nama, &profile.NoTelpon, &profile.Password)
+	var pengguna Pengguna
 
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err == sql.ErrNoRows {
+	queryRes := db.Table("pengguna").Select("email, nama, noTelpon, password").Where("id = ?", id).Find(&pengguna)
+	if queryRes.Error != nil {
 		response.SendErrorResponse(c, response.Response{
 			Message: "User ID not found",
 		})
 		return
 	}
 
+	if email == "" {
+		email = pengguna.Email
+	}
+
 	if nama == "" {
-		nama = profile.Nama
+		nama = pengguna.Nama
 	}
 
-	if noTelp == "" {
-		noTelp = profile.NoTelpon
+	if noTelpon == "" {
+		noTelpon = pengguna.NoTelpon
 	}
 
-	if pass == "" {
-		pass = profile.Password
+	if password == "" {
+		password = pengguna.Password
 	}
 
-	_, errQuery := db.Exec("UPDATE pengguna SET nama = ?, password = ?, noTelpon = ? WHERE id = ?",
-		nama,
-		pass,
-		noTelp,
-		id,
-	)
+	updateRes := db.Table("pengguna").Where("id = ?", id).Updates(map[string]interface{}{"email": email, "nama": nama, "noTelpon": noTelpon, "password": password})
+
+	if updateRes.Error != nil {
+		response.SendErrorResponse(c, response.Response{
+			Message: "Failed to update",
+		})
+		return
+	}
 
 	var res response.Response
-	if errQuery == nil {
-		res.Message = "Update Profile Success"
-		response.SendSuccessResponse(c, res)
-	} else {
-		res.Message = "Update Profile Failed"
-		response.SendErrorResponse(c, res)
-	}
+
+	res.Message = "Update user data success"
+	res.Data = updateRes.RowsAffected
+	response.SendSuccessResponse(c, res)
 }
 
 // func CheckOldPass(c *gin.Context) {

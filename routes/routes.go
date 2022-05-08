b1df3735e2ee93b873dd6211fa1b5053598870cb
NealsonW <nealsonwilliam@gmail.com>
2022-05-08 21:05:52 +0700

    menambah endpoint WalletTopUp

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"yukbayar-rpll-be/modules/pengguna"
+	"yukbayar-rpll-be/modules/walletTopUp"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -11,16 +12,27 @@ func Routes(route *gin.Engine, db *gorm.DB) {
 	penggunaRepository := pengguna.NewRepository(db)
 	penggunaService := pengguna.NewService(penggunaRepository)
 	penggunaController := pengguna.NewController(penggunaService)
+	topupRepository := walletTopUp.NewRepository(db)
+	topupService := walletTopUp.NewService(topupRepository)
+	topupController := walletTopUp.NewController(topupService)
 
 	root := route.Group("/")
 	{
 		root.POST("/register", penggunaController.Register)
 		root.GET("/login", penggunaController.Login)
+		root.GET("/check", penggunaController.CheckLogin)
 	}
 
 	users := route.Group("/users")
 	{
 		users.GET("/:id", penggunaController.GetUserData)
 		users.PUT("/:id", penggunaController.UpdateUser)
+
+	}
+
+	topups := route.Group("/topups")
+	{
+		topups.POST("/topup", topupController.InsertNewTopup)
+		topups.PUT("/:id", topupController.UpdateSaldoUser)
 	}
 }

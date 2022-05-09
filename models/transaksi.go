9eb850e20578521b10bf03beaf1242bc2a5e498d
Hilbert <88700493+hilberthp@users.noreply.github.com>
2022-05-10 02:23:02 +0800

    Fix getTransaksi

--- models/transaksi.go
+++ models/transaksi.go
@@ -7,7 +7,7 @@ type Transaksi struct {
 	ID_Pengguna      string    `form:"id_pengguna" json:"id_pengguna"`
 	Pengguna         string    `gorm:"column:nama" form:"nama" json:"nama"`
 	Varian           string    `gorm:"column:jenis" form:"varian" json:"varian"`
-	TotalHarga       int       `gorm:"column:harga" form:"totalHarga" json:"totalHarga"`
+	TotalHarga       int       `gorm:"column:totalHarga" form:"totalHarga" json:"totalHarga"`
 	TanggalTransaksi time.Time `gorm:"column:tanggal" form:"tanggal" json:"tanggal"`
 	Status           string    `form:"status" json:"status"`
 }

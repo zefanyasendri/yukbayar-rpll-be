6412ebc7ca56b6bd60edf0db4e27b2dadb6bfd48
NealsonW <nealsonwilliam@gmail.com>
2022-05-11 14:46:59 +0700

    Update variable

--- models/mitra.go
+++ models/mitra.go
@@ -7,7 +7,7 @@ type Mitra struct {
 	Email          string `form:"email" json:"email"`
 	NoTelpon       string `gorm:"type:varchar(255);column:noTelpon" form:"noTelpon" json:"noTelpon"`
 	PemilikBisnis  string `gorm:"type:varchar(255);column:pemilikBisnis" form:"pemilikBisnis" json:"pemilikBisnis"`
-	JenisUsaha     string `gorm:"type:varchar(255);column:jenisUsaha" form:"jenisUsaha" json:"jenisUsaha"`
+	BidangUsaha    string `gorm:"type:varchar(255);column:bidangUsaha" form:"bidangUsaha" json:"bidangUsaha"`
 	BadanUsaha     string `gorm:"type:varchar(255);column:badanUsaha" form:"badanUsaha" json:"badanUsaha"`
 	NamaPerusahaan string `gorm:"type:varchar(255);column:namaPerusahaan" form:"namaPerusahaan" json:"naamPerusahaan"`
 	//Layanan       []Layanan `form:"layanan" json:"layanan"`

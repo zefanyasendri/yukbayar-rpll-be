68a8916ab3bae408eaf8aec1a9eb4cb70629d0b3
NealsonW <nealsonwilliam@gmail.com>
2022-05-12 23:20:45 +0700

    Logic Fixes for TopUp

--- routes/routes.go
+++ routes/routes.go
@@ -63,7 +63,7 @@ func Routes(route *gin.Engine, db *gorm.DB) {
 	topups := route.Group("/topups")
 	{
 		topups.POST("/topup", topUpController.InsertNewTopup)
-		topups.PUT("/:id", topUpController.UpdateSaldoUser)
+		topups.PUT("/:idPengguna/:id", topUpController.UpdateSaldoUser)
 		topups.GET("/:id", topUpController.GetByPenggunaID)
 	}
 

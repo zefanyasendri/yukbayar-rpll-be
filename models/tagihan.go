bd1a55e5c9fd743af4e364519a1480b798a64211
NealsonW <nealsonwilliam@gmail.com>
2022-05-13 14:37:02 +0700

    Added Tagihan

--- models/tagihan.go
+++ models/tagihan.go
@@ -3,8 +3,8 @@ package models
 type Tagihan struct {
 	ID_Tagihan  string `form:"id_transaksi" json:"id_transaksi"`
 	ID_Pengguna string `form:"id_pengguna" json:"id_pengguna"`
-	ID_Kategori string `form:"id_kategori" json:"id_kategori"`
+	ID_Kategori string `gorm:"column:id_kategoriLayanan" form:"id_kategori" json:"id_kategori"`
 	No_Tagihan  string `gorm:"column:nomor_tagihan" form:"no_tagihan" json:"no_tagihan"`
 	Harga       int    `gorm:"column:harga" form:"harga" json:"harga"`
-	Status      string `form:"status" json:"status"`
+	Status      string `gorm:"default:'Pending'" form:"status" json:"status"`
 }
